2db97190d28adccf9ec15b7ad0fc23d0de49f151
vinayak birajdar <108874547+vinayakbirajdar@users.noreply.github.com>
2025-05-11 17:28:57 +0530

    Goggle fire base authentication implemnetation

---
+++ src/utils/firebase/firebaseConfig.js
@@ -0,0 +1,17 @@
+import auth from '@react-native-firebase/auth';
+import firebase from '@react-native-firebase/app';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCtkKGTk5tqMvfSxn2sTUwmzLOAF3ydfcg",
+    authDomain: "truf-booking-ca7dd.firebaseapp.com",
+    projectId: "truf-booking-ca7dd",
+    storageBucket: "truf-booking-ca7dd.firebasestorage.app",
+    messagingSenderId: "768754959579",
+    appId: "1:768754959579:android:7a5408fb7767f239a5f8fe",
+};
+
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+}
+
+export { auth };
