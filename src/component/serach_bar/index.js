1bc64699a667639761640d22f26071426af45bfb
vinayak birajdar <108874547+vinayakbirajdar@users.noreply.github.com>
2025-05-14 23:41:51 +0530

    Added a custom search bar and implemented in available screen

---
+++ src/component/serach_bar/index.js
@@ -0,0 +1,73 @@
+import React, { useState } from 'react';
+import {
+    View,
+    TextInput,
+    TouchableOpacity,
+    Text,
+    StyleSheet,
+    Keyboard,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+const CustomSearchBar = ({ placeholder = 'Search...', data = [], onSelectItem }) => {
+    const [searchText, setSearchText] = useState('');
+    const [filteredData, setFilteredData] = useState([]);
+
+    const handleSearch = (text) => {
+        setSearchText(text);
+        const filtered = data.filter((item) =>
+            item.toLowerCase().includes(text.toLowerCase())
+        );
+        setFilteredData(filtered);
+    };
+
+    const clearSearch = () => {
+        setSearchText('');
+        setFilteredData([]);
+        Keyboard.dismiss();
+    };
+
+    return (
+
+        <View style={styles.searchWrapper}>
+            <TextInput
+                style={styles.input}
+                placeholder={placeholder}
+                value={searchText}
+                onChangeText={handleSearch}
+            />
+            {searchText.length > 0 && (
+                <TouchableOpacity onPress={clearSearch} style={styles.iconButton}>
+                    <Icon name="close" size={25} color="red" />
+                </TouchableOpacity>
+            )}
+        </View>
+
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        marginh: 16,
+    },
+    searchWrapper: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        borderWidth: 1,
+        borderColor: '#ccc',
+        borderRadius: 8,
+        paddingHorizontal: 8,
+        backgroundColor: 'white'
+
+    },
+    input: {
+        flex: 1,
+        height: 50,
+        paddingHorizontal: 8,
+    },
+    iconButton: {
+        paddingHorizontal: 4,
+    },
+});
+
+export default CustomSearchBar;
--- src/screens/available_screen/index.js
+++ src/screens/available_screen/index.js
@@ -1,14 +1,58 @@
-import React from "react";
-import { SafeAreaView, Text, View } from "react-native";
+import React, { useState } from "react";
+import { FlatList, StatusBar, Text, TouchableOpacity, View } from "react-native";
+import CustomSearchBar from "../../component/serach_bar";
+import { SafeAreaView } from "react-native-safe-area-context";
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
+
+
+const sportOptions = [
+    { id: '1', name: 'All Sports' },
+    { id: '2', name: 'Football' },
+    { id: '3', name: 'Circket' },
+    { id: '4', name: 'Voleyball' },
+    { id: '5', name: 'Football' },
+]
 const AvailableScreen = () => {
+    const [selectedSportId, setSelectedSportId] = useState(null);
     return (
-        <SafeAreaView style={{ flex: 1 }}>
-            <View style={{ alignItems: 'center', justifyContent: 'center', flex: 1 }}>
-                <Text style={{ color: 'black' }}>
-                    Available Screen
-                </Text>
+        <SafeAreaView style={{ flex: 1, marginHorizontal: 14, marginTop: 5 }}>
+            <CustomSearchBar
+                placeholder="Search Trufs by name"
+            />
+            <View >
+                <FlatList
+                    data={sportOptions}
+                    horizontal
+                    showsHorizontalScrollIndicator={false}
+                    keyExtractor={(item, index) => index.toString()}
+                    renderItem={({ item }) => (
+                        <TouchableOpacity
+                            onPress={() => setSelectedSportId(item.id)}
+                            style={{
+                                marginRight: 10,
+                                paddingHorizontal: 20,
+                                paddingVertical: 10,
+                                backgroundColor: selectedSportId === item.id ? 'black' : 'white',
+                                borderRadius: 10
+                            }}
+                        >
+                            <Text style={{ color: selectedSportId === item.id ? 'white' : 'black', fontSize: 14 }}>{item.name}</Text>
+                        </TouchableOpacity>
+                    )}
+                    contentContainerStyle={{ marginTop: 10 }}
+                />
+                <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', marginTop: 10 }}>
+                    <Text style={{ fontWeight: 'bold', fontSize: 18, color: 'black' }}>All Trufs</Text>
+                    <TouchableOpacity >
+                        <Icon name='filter' size={30} />
+                    </TouchableOpacity>
+
+                </View>
+
             </View>
+
+
         </SafeAreaView>
     )
 
